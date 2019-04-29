cae840961860b6def06abad1bb1a9941c58fef77
relife957 <1401605581@qq.com>
2019-04-29 21:59:47 +0800

    update

--- experiment_three/main.py
+++ experiment_three/main.py
@@ -16,6 +16,7 @@ import os
 import tornado.ioloop
 import tornado.web
 import base64
+import tornado.websocket
 
 id = 1
 
@@ -28,6 +29,32 @@ class MainHandler(tornado.web.RequestHandler):
         image = base64.b64decode(image)
         with open(path, 'wb') as f:
             f.write(image)
+        print("收到一张图片!!!")
+        id += 1
+
+
+class LoginHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index2.html")
+
+
+# websocket连接控制
+class EchoWebSocket(tornado.websocket.WebSocketHandler):
+
+    def open(self):
+        print("有用户接入!!")
+
+    def on_close(self):
+        print("有用户离开!!")
+
+    def on_message(self, message):
+        global id
+        image = message
+        path = "./static/result/" + str(id) + ".png"
+        image = base64.b64decode(image)
+        with open(path, 'wb') as f:
+            f.write(image)
+        print("收到一张图片!!!")
         id += 1
 
 
@@ -38,7 +65,10 @@ def make_app():
         debug=True)
 
     return tornado.web.Application([
-        (r"/", MainHandler)
+        (r"/submit", MainHandler),
+        (r"/", LoginHandler),
+        (r'/(favicon.ico)', tornado.web.StaticFileHandler, {"path": "./favicon.ico"}),
+        (r"/ws", EchoWebSocket)
     ], **settings)
 
 
